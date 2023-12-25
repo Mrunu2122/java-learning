cab744e3b88cb39a8fd168e876723ef7337ddb17
Mrunu2122 <mrunalsoshte212208@gmail.com>
2023-12-25 16:03:46 +0530

    init

--- hello.java
+++ hello.java
@@ -1,5 +1,24 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.print("Hello Java");
+        // byte - 1 [-128 to 127]
+        // short - 2 
+        // int - 4
+        // long - 8
+        // float - 4
+        // double - 8
+        // char - 2 (a,b,c,d)
+        // boolean - 1 true/false
+
+        byte age = 30;
+        int phone = 1234567890;
+        long phone2 = 12345678900L;
+        float pi = 3.14F;
+        char letter = '@';
+        boolean isAdult = true;
+
+        // Non-primitive types
+        String name = new String ("Mrunal");
+        String friend = new String ("Dipti");
+        System.out.println(friend.length());
     }
 }
