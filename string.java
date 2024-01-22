62ea940a3b319974a74a233dbf056375e098dd50
Mrunu2122 <mrunalsoshte212208@gmail.com>
2024-01-22 20:23:47 +0530

    java.learning

--- string.java
+++ string.java
@@ -1,3 +1,20 @@
 public class string {
     
+    public static void main(String[] args) {
+        String name1 = "Mrunal";
+        String name2 = "dipti";
+        String name3 = name1 + " and " + name2;
+        System.out.println(name3);
+
+        // java is a zero based index langugage
+
+        String name = "Mrunal";
+        System.out.println(name.charAt(0));
+        System.out.println(name.charAt(1));
+
+        // replace
+        name = "dipti";
+        name2 = name.replace('i', 'b');
+        System.out.println(name2);
+    }
 }
